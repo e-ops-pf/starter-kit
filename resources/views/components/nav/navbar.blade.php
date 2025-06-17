ce2d477d6edd892552bf65fe200da39163c5d09e
samy <samy.barbi@yahoo.fr>
2025-06-17 10:00:05 -1000

    Tweak navbar

--- resources/views/components/nav/navbar.blade.php
+++ resources/views/components/nav/navbar.blade.php
@@ -1,21 +1,29 @@
+@props([
+    'menu' => null,
+    'brand' => null,
+    'user' => null,
+    'home' => '/',
+])
 <div class="navbar bg-primary text-primary-content shadow-sm">
     <div class="navbar-start">
         <!-- Mobile Dropdown -->
+        @if($menu)
         <div
                 class="dropdown"
         >
             <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
-                <x-heroicon-m-bars-4 class="h-5 w-5" href=""/>
+                <x-heroicon-m-bars-4 class="h-5 w-5"/>
             </div>
             <ul
                 class="menu menu-sm dropdown-content bg-primary rounded-box z-1 mt-3 w-52 p-2 shadow">
                 {{ $menu }}
             </ul>
         </div>
+        @endif
 
         <!-- Brand -->
         @if($brand)
-            <a class="btn btn-ghost text-xl">
+            <a class="btn btn-ghost text-xl" href="{{ $home }}">
                 {{ $brand }}
             </a>
         @endif
@@ -23,9 +31,11 @@
 
     <!-- Desktop Menu -->
     <div class="navbar-center hidden lg:flex">
+        @if($menu)
         <ul class="menu menu-horizontal px-1">
             {{ $menu }}
         </ul>
+        @endif
     </div>
 
     <!-- User -->
