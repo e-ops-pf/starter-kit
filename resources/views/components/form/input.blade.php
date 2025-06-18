7c291a887fff4eebad51d771b171a0fa6a81ee01
samy <samy.barbi@yahoo.fr>
2025-06-17 17:02:12 -1000

    Tweak file input error (multiple input bug)

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -24,5 +24,16 @@
         <x-sk::form.help>{{ $help }}</x-sk::form.help>
     @endif
 
-    <x-sk::form.error :name="$name" />
+        @if($type === 'file' && $multiple)
+            @foreach ($errors->get( rtrim($name, []) . '.*') as $messages)
+                @foreach ($messages as $message)
+                    <p class="text-sm text-error mt-1">
+                        {{ $message }}
+                    </p>
+                @endforeach
+            @endforeach
+        @else
+            <x-sk::form.input-error :name="$name" />
+        @endif
+
 </div>
