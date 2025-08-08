790e98e96c52579506ae2e9da9ae6fbf27e59b8e
samy <samy.barbi@yahoo.fr>
2025-08-07 18:49:04 -1000

    Fix dropdown not closing when clicking away

--- resources/views/components/nav/dropdown-item.blade.php
+++ resources/views/components/nav/dropdown-item.blade.php
@@ -3,7 +3,10 @@
 ])
 
 <li class="z-10">
-    <details>
+    <details
+            x-data
+            x-ref="dropdown"
+            @click.outside="$refs.dropdown.open = false">
         <summary>{{ $title }}</summary>
         <ul class="p-2 bg-primary">
             {{ $slot }}
